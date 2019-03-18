1209a557f59d89fc53ed87d2841b6d82050636b2
aisino-liqq <liquanqiang@aisino.com>
2019-03-18 19:48:01 +0800

    简单的服务

---
+++ src/main/java/com/user/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.user.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户
+ * 
+ * @author aisino
+ *
+ */
+@RestController
+@RequestMapping("user")
+public class UserController {
+
+	@Value("${server.port}")
+	private Integer port;
+	private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+	@PostMapping("getUser")
+	public String getUser(String name) {
+		logger.info("请求端口{}的用户{}", port, name);
+		return "请求端口" + port + "的用户" + name;
+	}
+
+}
